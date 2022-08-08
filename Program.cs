20e5a8557c8323f78bc156b2ea6a3fa4b69fb6b5
Stanley Goldman <Stanley.Goldman@gmail.com>
2022-08-08 12:24:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DiskInfoLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drives = DriveInfo.GetDrives();
+            foreach (var item in drives)
+            {
+                if (item.IsReady)
+                {
+                    Console.WriteLine($"Name: {item.Name}. DriveType: {item.DriveType}. AvailableFreeSpace: {item.AvailableFreeSpace}. TotalFreeSpace: {item.TotalFreeSpace}. TotalSize: {item.TotalSize}. DriveFormat: {item.DriveFormat}.");
+                }
+            }
+        }
+    }
+}
